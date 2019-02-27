731aa977d0747fd141e437bbe88f335c54255401
lijianzhen5 <lijianzhen1@jd.com>
2019-02-27 15:06:49 +0800

    使用redis Hyperloglog对对应的统计操作

--- bule-redis/src/main/java/com/bule/redis/hyperloglog/HyperloglogService.java
+++ bule-redis/src/main/java/com/bule/redis/hyperloglog/HyperloglogService.java
@@ -6,6 +6,8 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 /**
+ * 实现记录网站每天访问的独立IP数量这样的一个功能
+ * 主要解决去重的问题处理
  * Created by lijianzhen1 on 2019/2/27.
  */
 @Service
@@ -19,11 +21,11 @@ public class HyperloglogService {
     public void recodIp() {
         Jedis jedis = jedisPool.getResource();
 
-        jedis.pfadd("unique::ip::counter","192.189.199.3");
-        jedis.pfadd("unique::ip::counter","127.0.0.1");
-        jedis.pfadd("unique::ip::counter","127.0.0.1");
-        jedis.pfadd("unique::ip::counter","255.255.255.255");
+        jedis.pfadd("unique::ip::counter", "192.189.199.3");
+        jedis.pfadd("unique::ip::counter", "127.0.0.1");
+        jedis.pfadd("unique::ip::counter", "127.0.0.1");
+        jedis.pfadd("unique::ip::counter", "255.255.255.255");
         System.out.println(jedis.pfcount("unique::ip::counter"));
-        System.out.println(jedis.pfmerge("unique::ip::counterall","unique::ip::counter"));
+        System.out.println(jedis.pfmerge("unique::ip::counterall", "unique::ip::counter"));
     }
 }
--- bule-redis/src/test/java/com/bule/redis/hyperloglog/HyperloglogServiceTest.java
+++ bule-redis/src/test/java/com/bule/redis/hyperloglog/HyperloglogServiceTest.java
@@ -1,7 +1,6 @@
 package com.bule.redis.hyperloglog;
 
 import com.bule.redis.BaseTest;
-import com.bule.redis.bitmap.BitmapService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,13 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
  * 实现记录网站每天访问的独立IP数量这样的一个功能
  * Created by lijianzhen1 on 2019/2/27.
  */
-public class HyperloglogServiceTest extends BaseTest{
+public class HyperloglogServiceTest extends BaseTest {
 
     @Autowired
     private HyperloglogService hyperloglogService;
 
     @Test
-    public void TestrecodIp(){
+    public void TestrecodIp() {
 
         hyperloglogService.recodIp();
     }
